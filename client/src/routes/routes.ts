d2705bf4793b9fb6ee88a36e132a1d161bfdd196
Alan Escobedo <ingescobedoalan4@gmail.com>
2023-03-26 19:13:28 -0600

    Iniciando el curso y recursos

--- client/src/routes/routes.ts
+++ client/src/routes/routes.ts
@@ -1,5 +1,7 @@
 import { lazy, LazyExoticComponent } from 'react';
 
+import Base from '../Base';
+
 // import { LazyPage1, LazyPage2, LazyPage3 } from '../lazyload/pages';
 
 type JSXComponent = () => JSX.Element;
@@ -11,33 +13,23 @@ interface Route {
   name: string;
 }
 
-const Lazy1 = lazy(
-  () => import(/* webpackChunkName: "LazyPage1" */ '../lazyload/pages/LazyPage1'),
-);
-const Lazy2 = lazy(
-  () => import(/* webpackChunkName: "LazyPage2" */ '../lazyload/pages/LazyPage2'),
-);
-const Lazy3 = lazy(
-  () => import(/* webpackChunkName: "LazyPage3" */ '../lazyload/pages/LazyPage3'),
-);
-
 export const routes: Route[] = [
   {
     to: '/lazy1',
     path: 'lazy1',
-    Component: Lazy1,
+    Component: Base,
     name: 'Lazy-1',
   },
   {
     to: '/lazy2',
     path: 'lazy2',
-    Component: Lazy2,
+    Component: Base,
     name: 'Lazy-2',
   },
   {
     to: '/lazy3',
     path: 'lazy3',
-    Component: Lazy3,
+    Component: Base,
     name: 'Lazy-3',
   },
 ];
